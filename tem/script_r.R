f74e9b7535f2f397cc040ba113e47d0b41525577
Daniel Rode <d.rode@protonmail.com>
2026-02-11 01:24:31 -0700

    shtdn auto-commit

--- tem/script_r.R
+++ tem/script_r.R
@@ -14,6 +14,12 @@
 # ...
 
 
+# Constants
+HELP_TEXT = "Usage: this.R [OPTIONS] IN_DIR OUT_DIR\n"
+
+
+# Main
+
 # Parse command line arguments
 # TODO this was snip copied from another script and needs to be made general
 pos_args = character()
@@ -25,33 +31,33 @@ debug_save_extents = FALSE
 args = commandArgs(trailingOnly = TRUE)
 counter = 1
 while (TRUE) {
-  a = args[counter]
-  if (is.na(a)) {
-    break
-  } else if (!startsWith(a, "-")) {
-    pos_args = c(pos_args, a)
-    counter = counter + 1
-  } else if (a == "-d" || a == "--dtm-path") {
-    dtm_path = args[counter + 1]
-    counter = counter + 1
-  } else if (a == "-t" || a == "--max-threads") {
-    max_threads = as.integer(args[counter + 1])
-    counter = counter + 1
-  } else if (a == "-c" || a == "--collection-cache-path") {
-    # Path to RDS file that contains a cached/pre-generated lidR
-    # LAS_CATALOG object
-    collection_cache_path = args[counter + 1]
-    counter = counter + 1
-  } else if (a == "--debug-save-extents") {
-    debug_save_extents = TRUE
-  } else {
-    stop("Invalid flag: ", a)
-  }
+    a = args[counter]
+    if (is.na(a)) {
+        break
+    } else if (!startsWith(a, "-")) {
+        pos_args = c(pos_args, a)
+        counter = counter + 1
+    } else if (a == "-d" || a == "--dtm-path") {
+        dtm_path = args[counter + 1]
+        counter = counter + 1
+    } else if (a == "-t" || a == "--max-threads") {
+        max_threads = as.integer(args[counter + 1])
+        counter = counter + 1
+    } else if (a == "-c" || a == "--collection-cache-path") {
+        # Path to RDS file that contains a cached/pre-generated lidR
+        # LAS_CATALOG object
+        collection_cache_path = args[counter + 1]
+        counter = counter + 1
+    } else if (a == "--debug-save-extents") {
+        debug_save_extents = TRUE
+    } else {
+        stop("Invalid flag: ", a)
+    }
 }
 
 if (length(pos_args) != 6) {
-  cat(HELP_TEXT)
-  quit(status = 1)
+    cat(HELP_TEXT)
+    quit(status = 1)
 }
 ctg_pth = pos_args[1]
 out_dir = pos_args[2]
@@ -64,7 +70,7 @@ cat("Args: ")
 dput(args)
 
 if (! file.exists(ctg_pth)) {
-  stop("Path does not exist: ", ctg_pth)
+    stop("Path does not exist: ", ctg_pth)
 }
 dir.create(out_dir, showWarnings = FALSE, recursive = FALSE, mode = "0700")
 tile_bound_max_x = tile_bound_min_x + tile_length
